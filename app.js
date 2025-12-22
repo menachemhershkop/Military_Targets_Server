51f4fbcd8c433f5c33e6d2dece7646838c21e5f7
mendy hershkop <hers.mm@gmail.com>
2025-12-22 17:41:49 +0200

    create main progrem

---
+++ app.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import morgan from 'morgan'
+import {promises as fs} from 'fs'
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms'));
+app.use("/", (req, res, next) => {
+    const time = new Date().toDateString()
+	res.setHeader("X-Server-Start-Time", time);
+    console.log(res.getHeaders());
+     	
+	next();
+});
+
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/health', (req, res) => {
+    const time = new Date().toISOString()
+  res.send({
+  "status": "ok",
+  "serverTime": time
+})
+
+})
+
+app.get('/briefing', (req, res)=> {
+    const token = 'Golani';
+    
+    if (req.header('Client-Unit') == token){
+        res.send({
+  "unit": "Golani",
+  "message": "briefing delivered"
+}
+)
+    }
+    else{
+        res.status(400).send("Not found")}
+}
+)
+
+app.get('/targets/:id', async (req,res)=> {
+    const param = req.params.id;
+    let file = await fs.readFile("data/targets.json", "utf8")
+    const users = JSON.parse(file)
+    try{
+    users.forEach((elemnt) => {if (elemnt.id == param){
+        res.send(elemnt)
+    }
+    else{
+        res.status(404).send("Not found")
+    }
+    })}
+   catch(error){
+    res.status(404).send("Not found")
+   }
+})
+
+app.get('/targets', async (req,res)=> {
+    const {region, status, minPriority} = req.query
+    let file = await fs.readFile("data/targets.json", "utf8")
+    const users = JSON.parse(file)
+    users.forEach((element)=> {if(element.region==region|| element.status==status|| element.priority== minPriority){
+        res.send(element)}
+    else{
+        res.status(400).send("Nothing found. try another search...")
+    }
+    })
+})
